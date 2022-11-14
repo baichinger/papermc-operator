28924833b61b7320f1d7dca1f60b4fab7d6adb7d
Bernhard Aichinger <bernhard.aichinger@dynatrace.com>
2022-11-14 23:16:45 +0100

    Add basic interface documentation

--- pkg/papermc/client/client.go
+++ pkg/papermc/client/client.go
@@ -1,6 +1,12 @@
 package client
 
+// Client is the client for interacting with the Paper MC API. Only minimal functionality is provided for downloading
+// new versions of Paper.
 type Client interface {
+	// GetBuildForVersion determines the build id for a given version.
 	GetBuildForVersion(version string) (int, error)
+
+	// GetUrlForVersionBuildDownload determines the download URL of a given version/build. The URL returned points
+	// to the corresponding JAR file.
 	GetUrlForVersionBuildDownload(version string, build int) (string, error)
 }
